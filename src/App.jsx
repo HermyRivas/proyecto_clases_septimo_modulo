5597dcf10cda13a5268979e761f7323185750a57
Hermy Rivas <hermy.alfonso.rivas@clases.edu.sv>
2026-07-03 18:55:33 -0600

    Modulo 7

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import Encabezado from './components/Encabezado';
+import TarjetaAlumno from './components/TarjetaAlumno';
+import ListaAlumnos from './components/ListaAlumnos';
+import './App.css';
+ 
+function App() {
+  const nombreEstudiante = 'Vic Flores';
+  const horaActual = new Date().getHours();
+ 
+  return (
+    <div>
+      <Encabezado />
+ 
+      <ListaAlumnos />
+    </div>
+  );
+}
+ 
+export default App;
